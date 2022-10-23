dba2d99df5f3fd65e990fbe186f63fdd95f51271
Ruberald <sajidhossain909@gmail.com>
2022-10-23 11:22:00 +0530

    Added files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# BODY MASS INDEX
+# BODY MASS INDEX GUI
 
-This a simple tool to calculate the body mass index of a person
+This a simple GUI tool to calculate the body mass index of a person
--- main.cc
+++ main.cc
@@ -2,22 +2,37 @@
 #include <string>
 #include <utility>
 #include "Calculation.h"
+#include "app.h"
 
 typedef std::pair<float, std::string> Result;
 
 extern Result calculateBMI(float weight, float height);
 
-int main()
+void DemoFrame::OnPress(wxCommandEvent& event)
 {
-	float weight, height;
+	float height = std::stof(std::string((textctrlHeight->GetLineText(0)).mb_str()));
+	float weight = std::stof(std::string((textctrlWeight->GetLineText(0)).mb_str()));
 
-	std::cout << "Enter weight: ";
-	std::cin >> weight;
+	Result res = calculateBMI(weight, height);
+	std::string res_string = std::to_string(res.first) + "\n" + res.second;
+	wxString res_wxstring(res_string.c_str(), wxConvUTF8);
 
-	std::cout << "Enter height: ";
-	std::cin >> height;
-
-	Result result = calculateBMI(weight, height);
-	std::cout << "Your BMI is " << result.first << std::endl;
-	std::cout << "Result: " << result.second << std::endl;
+	result->SetLabelText(res_wxstring);
 }
+
+// int main()
+// {
+// 	float weight, height;
+
+// 	std::cout << "Enter weight: ";
+// 	std::cin >> weight;
+
+// 	std::cout << "Enter height: ";
+// 	std::cin >> height;
+
+// 	Result result = calculateBMI(weight, height);
+// 	std::cout << "Your BMI is " << result.first << std::endl;
+// 	std::cout << "Result: " << result.second << std::endl;
+// }
+
+IMPLEMENT_APP(MainApp);
